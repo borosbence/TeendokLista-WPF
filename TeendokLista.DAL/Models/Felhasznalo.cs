649432ea5d8449f2ef88939449d0697fcf8cfc99
Boros Bence <hello@bitben.hu>
2021-09-30 23:02:17 +0200

    db kapcsolat

---
+++ TeendokLista.DAL/Models/Felhasznalo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeendokLista.DAL.Models
+{
+    // [Table("Felhasznalok")]
+    [Index(nameof(Felhasznalonev), IsUnique = true)]
+    public class Felhasznalo
+    {
+        public Felhasznalo()
+        {
+            //Feladatok = new HashSet<Feladat>();
+        }
+
+        public int Id { get; set; }
+        [Required]
+        public string Felhasznalonev { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Jelszo { get; set; }
+
+        //public virtual ICollection<Feladat> Feladatok { get; set; }
+    }
+}
---
+++ TeendokLista.DAL/Repositories/FelhasznaloRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeendokLista.DAL.Models;
+
+namespace TeendokLista.DAL.Repositories
+{
+    public class FelhasznaloRepository
+    {
+        private TeendokContext db = new TeendokContext();
+        
+        public async Task<bool> Authenticate(string username, string password)
+        {
+            // Ezzel a felhasználónévvel létezik e rekord
+            var dbUser = await db.Felhasznalok.SingleOrDefaultAsync(x => x.Felhasznalonev.Equals(username)).ConfigureAwait(false);
+            if (dbUser != null)
+            {
+                // Só lekérése titkosításhoz
+                //var salt = dbUser.id;
+                // Begépelt jelszó titkosítása
+                //var password = Hash.Encrypt(view.Password + salt);
+
+                // Rekord keresése
+                var user = await db.Felhasznalok.SingleOrDefaultAsync(x => x.Felhasznalonev.Equals(username) && x.Jelszo.Equals(password)).ConfigureAwait(false);
+                if (user != null)
+                {
+                    return true;
+                    // CurrentUser.Id = user.id;
+                }
+                else
+                {
+                    // Hibás login
+                    return false;
+                }
+            }
+            else
+            {
+                // Felhasználó nem létezik
+                return false;
+            }
+        }
+    }
+}
